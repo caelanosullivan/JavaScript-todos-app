a427521f283475f3c037bc2de1cabe2e7d31961e
Caelan O'Sullivan <caelanosullivan@gmail.com>
2018-02-03 20:04:21 -0600

    Added toggle button, variables to call buttons, and event listeners to run functions

--- todo-list.js
+++ todo-list.js
@@ -1,81 +1,86 @@
 var todoList = {
-  
+
     todos: [],
-    
+
     displayTodos: function() {
-       if (this.todos.length === 0) {
-        console.log('Your todo list is empty');
-       } else {
-          console.log('My Todos:');
-          for (var i = 0; i < this.todos.length; i++) {
-            if (this.todos[i].completed === true) {
-              console.log('(x)', this.todos[i].todoText);
-            } else {
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty');
+        } else {
+            console.log('My Todos:');
+            for (var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                console.log('(x)', this.todos[i].todoText);
+                } else {
                 console.log('( )', this.todos[i].todoText);
+                }
             }
-          }
-       }
+        }
     },
-    
+
     addTodo: function(todoText) {
-      this.todos.push({
+        this.todos.push({
         todoText: todoText,
         completed: false
-      });
-      this.displayTodos();
+        });
+        this.displayTodos();
     },
-    
+
     changeTodo: function(position, todoText) {
-      //this.todos[position] = todoText;
-      // change just the property todoText
-      this.todos[position].todoText = todoText
-      this.displayTodos();
+        //this.todos[position] = todoText;
+        // change just the property todoText
+        this.todos[position].todoText = todoText
+        this.displayTodos();
     },
-    
+
     deleteTodo: function(position) {
-      this.todos.splice(position, 1);
-      this.displayTodos();
+        this.todos.splice(position, 1);
+        this.displayTodos();
     },
-    
+
     toggleCompleted: function(position) {
-      // this todo variable just saves typing two lines down (see below)
-      var todo = this.todos[position];
-      todo.completed = !todo.completed;
-      // var flower = this.todos[position];
-      // flower.completed = !flower.completed;
-      this.displayTodos();
+        // this todo variable just saves typing two lines down (see below)
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        // var flower = this.todos[position];
+        // flower.completed = !flower.completed;
+        this.displayTodos();
     },
-    
+
     toggleAll: function() {
-      var totalTodos = this.todos.length;
-      var completedTodos = 0;
-      
-      //Get number of completed todos
-      for (var i = 0; i < totalTodos; i++) {
-        if (this.todos[i].completed === true) {
-          completedTodos++;
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        
+        //Get number of completed todos
+        for (var i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
         }
         
-      // Case 1: If all true (complete), make all false (incomplete)
-      if (completedTodos === totalTodos) {
+        // Case 1: If all true (completed), make all false (not completed)
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++) {
+            this.todos[i].completed = false;
+            }
+        // Case 2: Else make all true (completed) 
+        } else {
         for (var i = 0; i < totalTodos; i++) {
-          this.todos[i].completed = false;
+            this.todos[i].completed = true;
+            }
         }
-      // Case 2:else: make all true (compl) 
-      } else {
-        for (var i = 0; i < totalTodos; i++) {
-          this.todos[i].completed = true;
+        this.displayTodos();
         }
-      }
-      this.displayTodos();
     }
-  }
-  }
 
-// Select button using getElementById method (using HTML id)
-  var displayTodosButton = document.getElementById('displayTodosButton');
+// Select buttons as variables using getElementById method (using HTML id)
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton')
 
-// Add click event listener to run display function
+// Add click event listeners to run respective functions
   displayTodosButton.addEventListener('click', function() {
     todoList.displayTodos();
+  });
+
+  toggleAllButton.addEventListener('click', function() {
+      todoList.toggleAll();
   });
